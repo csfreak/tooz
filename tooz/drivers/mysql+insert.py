c7c24adc777dbd2db4dc87b8d810459313bb86d8
Jason Ross <jason.ross@viasat.com>
2016-06-30 14:17:38 -0500

    Add error handling

--- tooz/drivers/mysql+insert.py
+++ tooz/drivers/mysql+insert.py
@@ -55,6 +55,31 @@ class MySQLLock(locking.Lock):
                     cur.commit()
                     self.acquired = True
                     return True
+            except pymysql.ProgrammingError as e:
+                try:
+                    cur.execute("""CREATE TABLE 'tooz_locks' (
+                                   'name' varchar(255) NOT NULL DEFAULT '',
+                                   'created_at' timestamp NOT NULL
+                                   DEFAULT '0000-00-00 00:00:00',
+                                   'updated_at' timestamp NOT NULL
+                                   DEFAULT CURRENT_TIMESTAMP
+                                   ON UPDATE CURRENT_TIMESTAMP,
+                                   'created_by' varchar(255) NOT NULL DEFAULT ''
+                                   , PRIMARY KEY ('name'))
+                                   ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
+                    cur.commit()
+                    raise _retry.Retry                    
+                except pymyql.MySQLError as e:
+                    coordination.raise_with_cause(
+                        coordination.ToozError,
+                        encodeutils.exception_to_unicode(e),
+                        cause=e)
+            except pymysql.IntegrityError as e:
+                coordination.raise_with_cause(
+                    coordination.LockAcquireFailed,
+                    encodeutile.exception_to_unicode(e),
+                    cause=e)
+                
             except pymysql.MySQLError as e:
                 coordination.raise_with_cause(
                     coordination.ToozError,
@@ -122,33 +147,17 @@ class MySQLDriver(coordination.CoordinationDriver):
     def _start(self):
         self._conn = MySQLDriver.get_connection(self._parsed_url,
                                                 self._options)
-        self.check_db()
 
     def _stop(self):
+        with self._conn as cur:
+                cur.execute("""DELETE FROM `tooz_locks`
+                            `created_by` = %s;
+                            """ % (self.member_id))
+                cur.commit()
         self._conn.close()
 
     def get_lock(self, name):
         return MySQLLock(name, self.member_id, self._parsed_url, self._options)
-
-    def check_db(self):
-        try:
-            with self._conn as cur:
-                cur.execute("SHOW TABLES LIKE 'tooz_locks'")
-                if cur.fetchall().len() is 0:
-                    cur.execute("""CREATE TABLE 'tooz_locks' (
-                                   'name' varchar(255) NOT NULL DEFAULT '',
-                                   'created_at' timestamp NOT NULL
-                                   DEFAULT '0000-00-00 00:00:00',
-                                   'updated_at' timestamp NOT NULL
-                                   DEFAULT CURRENT_TIMESTAMP
-                                   ON UPDATE CURRENT_TIMESTAMP,
-                                   'created_by' varchar(255) NOT NULL DEFAULT '',
-                                   PRIMARY KEY ('name'))
-                                   ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
-        except pymysql.MySQLError as e:
-            coordination.raise_with_cause(coordination.ToozError,
-                                          encodeutils.exception_to_unicode(e),
-                                          cause=e)
 
     @staticmethod
     def watch_join_group(group_id, callback):
